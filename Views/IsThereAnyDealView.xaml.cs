f9b2f6f8f4d947a587e68f48b916769003f1e403
Lacro59 <lacro59@gmail.com>
2020-07-24 13:07:36 +0200

    Add search options

--- Views/IsThereAnyDealView.xaml.cs
+++ Views/IsThereAnyDealView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using IsThereAnyDeal.Clients;
 using IsThereAnyDeal.Models;
+using Newtonsoft.Json;
 using Playnite.Controls;
 using Playnite.SDK;
 using PluginCommon;
@@ -18,21 +19,29 @@ namespace IsThereAnyDeal.Views
     public partial class IsThereAnyDealView : WindowBase
     {
         private static readonly ILogger logger = LogManager.GetLogger();
+        private readonly IsThereAnyDealSettings settings;
 
         public string CurrencySign { get; set; }
         public string PlainSelected { get; set; }
+
+        private List<Wishlist> lbWishlistItems = new List<Wishlist>();
+        private List<string> SearchStores = new List<string>();
+        private int SearchPercentage = 0;
 
         public IsThereAnyDealView(IPlayniteAPI PlayniteApi, string PluginUserDataPath, IsThereAnyDealSettings settings, string PlainSelected = "")
         {
             InitializeComponent();
             this.PlainSelected = PlainSelected;
+            this.settings = settings;
 
             // Load data
+            dpData.IsEnabled = false;
             var task = Task.Run(() => LoadData(PlayniteApi, PluginUserDataPath, settings, PluginUserDataPath))
                 .ContinueWith(antecedent => 
                 {
                     Application.Current.Dispatcher.Invoke(new Action(() => {
-                        lbWishlist.ItemsSource = antecedent.Result;
+                        lbWishlistItems = antecedent.Result;
+                        lbWishlist.ItemsSource = lbWishlistItems;
                         if (!PlainSelected.IsNullOrEmpty())
                         {
                             int index = 0;
@@ -48,12 +57,32 @@ namespace IsThereAnyDeal.Views
                             }
                         }
                         DataLoadWishlist.Visibility = Visibility.Collapsed;
+                        dpData.IsEnabled = true;
                     }));
                 });
+
+            SetFilterStore();
 
             DataContext = this;
         }
 
+        private void SetFilterStore()
+        {
+            List<ListStore> FilterStoreItems = new List<ListStore>();
+            if (settings.EnableSteam)
+            {
+                FilterStoreItems.Add(new ListStore { StoreName = "Steam", IsCheck = false });
+            }
+            if (settings.EnableGog)
+            {
+                FilterStoreItems.Add(new ListStore { StoreName = "GOG", IsCheck = false });
+            }
+            if (settings.EnableHumble)
+            {
+                FilterStoreItems.Add(new ListStore { StoreName = "Humble", IsCheck = false });
+            }
+            FilterStore.ItemsSource = FilterStoreItems;
+        }
 
         private async Task<List<Wishlist>> LoadData (IPlayniteAPI PlayniteApi, string PluginUserDataPath, IsThereAnyDealSettings settings, string PlainSelected = "")
         {
@@ -90,5 +119,85 @@ namespace IsThereAnyDeal.Views
             Tools.DesactivePlayniteWindowControl(this);
             lbWishlist.ScrollIntoView(lbWishlist.SelectedItem);
         }
+
+
+        // Get list
+        private void GetListGame()
+        {
+            lbWishlist.ItemsSource = lbWishlistItems.FindAll(
+                x => x.ItadBestPrice.price_cut >= SearchPercentage
+            );
+
+            if (!TextboxSearch.Text.IsNullOrEmpty() && SearchStores.Count != 0)
+            {
+                lbWishlist.ItemsSource = lbWishlistItems.FindAll(
+                    x => x.Name.ToLower().IndexOf(TextboxSearch.Text) > -1 && SearchStores.Contains(x.StoreName)
+                );
+            }
+
+            if (!TextboxSearch.Text.IsNullOrEmpty())
+            {
+                lbWishlist.ItemsSource = lbWishlistItems.FindAll(
+                    x => x.Name.ToLower().IndexOf(TextboxSearch.Text) > -1
+                );
+            }
+
+            if (SearchStores.Count != 0)
+            {
+                lbWishlist.ItemsSource = lbWishlistItems.FindAll(
+                    x => SearchStores.Contains(x.StoreName)
+                );
+            }
+        }
+
+        // Search by name
+        private void TextboxSearch_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            GetListGame();
+        }
+
+        // Search by store
+        private void ChkStore_Checked(object sender, RoutedEventArgs e)
+        {
+            FilterCbStore((CheckBox)sender);
+        }
+        private void ChkStore_Unchecked(object sender, RoutedEventArgs e)
+        {
+            FilterCbStore((CheckBox)sender);
+        }
+        private void FilterCbStore(CheckBox sender)
+        {
+            FilterStore.Text = "";
+
+            if ((bool)sender.IsChecked)
+            {
+                SearchStores.Add((string)sender.Content);
+            }
+            else
+            {
+                SearchStores.Remove((string)sender.Content);
+            }
+
+            if (SearchStores.Count != 0)
+            {
+                FilterStore.Text = String.Join(", ", SearchStores);
+            }
+
+            GetListGame();
+        }
+
+        // Search percentage
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SearchPercentage = (int)((Slider)sender).Value;
+            GetListGame();
+        }
+    }
+
+
+    public class ListStore
+    {
+        public string StoreName { get; set; }
+        public bool IsCheck { get; set; }
     }
 }
