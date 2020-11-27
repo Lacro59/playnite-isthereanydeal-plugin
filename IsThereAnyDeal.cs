86bd166e30cc7a3d1442964e01d9de3f2829b9e6
Lacro59 <lacro59@gmail.com>
2020-11-27 14:42:19 +0100

    Minor updates

--- IsThereAnyDeal.cs
+++ IsThereAnyDeal.cs
@@ -34,7 +34,7 @@ namespace IsThereAnyDeal
             string pluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             // Add plugin localization in application ressource.
-            PluginCommon.Localization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
+            PluginCommon.PluginLocalization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
             // Add common in application ressource.
             PluginCommon.Common.Load(pluginFolder);
 
