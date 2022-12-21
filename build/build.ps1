7397a7800e908bd9e987e3c783e0e3e8e7e815df
Lacro59 <lacro59@gmail.com>
2022-12-21 10:45:40 +0100

    updated build

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,7 @@
 param(
 	[string]$ConfigurationName, 
-	[string]$OutDir
+	[string]$OutDir,
+	[string]$SolutionDir 
 )
 
 
@@ -35,12 +36,43 @@ $OutDirPath = (Join-Path $OutDir "..")
 
 if ($ConfigurationName -eq "release") 
 {
-	if (Test-Path $ToolboxPath)
+	$Version = ""
+	foreach($Line in Get-Content (Join-Path $SolutionDir  "extension.yaml")) 
 	{
-		& $ToolboxPath "pack" $OutDir $OutDirPath
+		if($Line -imatch "Version:")
+		{
+			$Version = $Line
+		}
 	}
-	else 
+
+	$Manifest = (Join-Path $SolutionDir  "..\manifest\")
+	$YmlFile = Get-ChildItem -Path $Manifest -Filter *.yaml | Select-Object -First 1
+	$Manifest = (Join-Path $Manifest $YmlFile.Name)
+
+	$Result = Get-Content $Manifest
+	if($Result -imatch $Version)
 	{
-		Write-Error "toolbox.exe not find."
-	}	
+		if (Test-Path $ToolboxPath)
+		{
+			& $ToolboxPath "pack" $OutDir $OutDirPath
+		
+			$Result = & $ToolboxPath "verify" "installer" $Manifest
+			if($Result -imatch "Installer manifest passed verification")
+			{		
+				
+			}
+			else 
+			{
+				echo $Result
+			}		
+		}
+		else 
+		{
+			echo "toolbox.exe not find."
+		}	
+	}
+	else
+	{
+		echo "Manifest not contains actual version"
+	}
 }
