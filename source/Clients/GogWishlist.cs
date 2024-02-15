1db1de44c449a40d08ed67a75ba54ebf129ff1fc
Lacro59 <lacro59@gmail.com>
2024-02-15 19:13:27 +0100

    optimized GOG wishlist

--- source/Clients/GogWishlist.cs
+++ source/Clients/GogWishlist.cs
@@ -1,46 +1,43 @@
 ﻿using IsThereAnyDeal.Models;
 using Playnite.SDK;
-using Playnite.SDK.Data;
 using CommonPluginsShared;
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Text;
-using AngleSharp.Parser.Html;
-using AngleSharp.Dom.Html;
-using CommonPlayniteShared.PluginLibrary.GogLibrary.Models;
 using IsThereAnyDeal.Models.Api;
+using CommonPluginsStores.Gog;
+using CommonPluginsStores.Models;
+using System.Collections.ObjectModel;
 
 namespace IsThereAnyDeal.Services
 {
     public class GogWishlist : GenericWishlist
     {
-        protected static GogAccountClientExtand _GogAPI;
-        internal static GogAccountClientExtand GogAPI
+        protected static GogApi _GogApi;
+        internal static GogApi GogApi
         {
             get
             {
-                if (_GogAPI == null)
+                if (_GogApi == null)
                 {
-                    _GogAPI = new GogAccountClientExtand(WebViewOffscreen);
+                    _GogApi = new GogApi("IsThereAnyDeal");
                 }
-                return _GogAPI;
+                return _GogApi;
             }
 
-            set => _GogAPI = value;
+            set => _GogApi = value;
         }
 
 
         public GogWishlist(IsThereAnyDeal plugin) : base(plugin, "GOG")
         {
-            ExternalPlugin = PlayniteTools.ExternalPlugin.EpicLibrary;
+            ExternalPlugin = PlayniteTools.ExternalPlugin.GogLibrary;
         }
 
         internal override List<Wishlist> GetStoreWishlist(List<Wishlist> cachedData)
         {
             Logger.Info($"Load data from web for {ClientName}");
 
-            if (!GogAPI.GetIsUserLoggedIn())
+            if (!GogApi.IsUserLoggedIn)
             {
                 Logger.Warn($"{ClientName}: Not authenticated");
                 API.Instance.Notifications.Add(new NotificationMessage(
@@ -54,87 +51,28 @@ namespace IsThereAnyDeal.Services
                 return cachedData;
             }
 
+            IsThereAnyDealApi isThereAnyDealApi = new IsThereAnyDealApi();
             List<Wishlist> wishlists = new List<Wishlist>();
-            bool HasError = false;
-            string ResultWeb = string.Empty;
+            ObservableCollection<AccountWishlist> accountWishlist = GogApi.GetWishlist(GogApi.CurrentAccountInfos);
 
-            Logger.Info($"Get GOG Wishlist with api");
-
-            // Get wishlist
-            ResultWeb = GogAPI.GetWishList();
-
-            // Get game information for wishlist
-            if (!ResultWeb.IsNullOrEmpty())
+            accountWishlist.ForEach(x =>
             {
-                // Not connected
-                if (ResultWeb.Contains("id=\"login_username\""))
+                GameLookup gamesLookup = isThereAnyDealApi.GetGamesLookup(x.Name).GetAwaiter().GetResult();
+                wishlists.Add(new Wishlist
                 {
-                    Logger.Warn($"GOG is disconnected");
-                }
-                else
-                {
-                    try
-                    {
-                        GogWishlistResult resultObj = Serialization.FromJson<GogWishlistResult>(ResultWeb);
-                        foreach (dynamic gameWishlist in resultObj.wishlist)
-                        {
-                            if ((bool)gameWishlist.Value)
-                            {
-                                string StoreId = gameWishlist.Name;
-
-                                Wishlist wishlist = GetGameData(StoreId);
-                                if (wishlist != null)
-                                {
-                                    wishlists.Add(wishlist);
-                                }
-                                else
-                                {
-                                    Logger.Warn($"GOG wishlist is incomplet");
-                                }
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Common.LogError(ex, false, $"Error in parse GOG wishlist", true, "IsThereAnyDeal");
-                        HasError = true;
-                    }
-                }
-            }
-
-            if (HasError)
-            {
-                Logger.Info($"Get GOG Wishlist without api");
-
-                // Get wishlist
-                ResultWeb = GogAPI.GetWishListWithoutAPI();
-
-                // Get game information for wishlist
-                if (!ResultWeb.IsNullOrEmpty())
-                {
-                    HtmlParser parser = new HtmlParser();
-                    IHtmlDocument HtmlRequirement = parser.Parse(ResultWeb);
-
-                    foreach (var el in HtmlRequirement.QuerySelectorAll(".product-row-wrapper .product-state-holder"))
-                    {
-                        string StoreId = el.GetAttribute("gog-product");
-
-                        Wishlist wishlist = GetGameData(StoreId);
-                        if (wishlist != null)
-                        {
-                            wishlists.Add(wishlist);
-                        }
-                        else
-                        {
-                            Logger.Warn($"GOG wishlist is incomplet - StoreId: {StoreId}");
-                        }
-                    }
-                }
-                else
-                {
-                    return cachedData;
-                }
-            }
+                    StoreId = x.Id,
+                    StoreName = "GOG",
+                    ShopColor = GetShopColor(),
+                    StoreUrl = x.Link,
+                    Name = x.Name,
+                    SourceId = PlayniteTools.GetPluginId(ExternalPlugin),
+                    ReleaseDate = x.Released,
+                    Added = x.Added,
+                    Capsule = x.Image,
+                    Game = gamesLookup.Found ? gamesLookup.Game : null,
+                    IsActive = true
+                });
+            });
 
             wishlists = SetCurrentPrice(wishlists);
             SaveWishlist(wishlists);
@@ -143,45 +81,7 @@ namespace IsThereAnyDeal.Services
 
         public override bool RemoveWishlist(string StoreId)
         {
-            return GogAPI.RemoveWishList(StoreId);
-        }
-
-
-        private Wishlist GetGameData(string StoreId)
-        {
-            IsThereAnyDealApi isThereAnyDealApi = new IsThereAnyDealApi();
-
-            //Download game information
-            string url = string.Format(@"https://api.gog.com/products/{0}", StoreId);
-            string ResultWeb = Web.DownloadStringData(url).GetAwaiter().GetResult();
-            try
-            {
-                ProductApiDetail resultObjGame = Serialization.FromJson<ProductApiDetail>(ResultWeb);
-                DateTime ReleaseDate = resultObjGame.release_date == null ? default : (DateTime)resultObjGame.release_date;
-                string Name = resultObjGame.title;
-                string Capsule = "http:" + resultObjGame.images.logo2x;
-
-                GameLookup gamesLookup = isThereAnyDealApi.GetGamesLookup(Name).GetAwaiter().GetResult();
-
-                return new Wishlist
-                {
-                    StoreId = StoreId,
-                    StoreName = "GOG",
-                    ShopColor = GetShopColor(),
-                    StoreUrl = resultObjGame.links.product_card,
-                    Name = WebUtility.HtmlDecode(Name),
-                    SourceId = PlayniteTools.GetPluginId(ExternalPlugin),
-                    ReleaseDate = ReleaseDate.ToUniversalTime(),
-                    Capsule = Capsule,
-                    Game = gamesLookup.Found ? gamesLookup.Game : null,
-                    IsActive = true
-                };
-            }
-            catch (Exception ex)
-            {
-                Common.LogError(ex, false, $"Failed to download game information for {StoreId}", true, "IsThereAnyDeal");
-                return null;
-            }
+            return GogApi.RemoveWishlist(StoreId);
         }
     }
 }
